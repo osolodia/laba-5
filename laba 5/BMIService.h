5d58db205fbe29fdfc7516af8da9f6f5848bf171
Oksi <osolodia@yandex.ru>
2023-12-29 13:29:20 +0700

    Oбработкa ошибок ввода данных пользователем

--- laba 5/BMIService.h
+++ laba 5/BMIService.h
@@ -10,31 +10,47 @@ private:
     Statistics statistics;
     Report report;    
 public:
-
     void inputUserInfo() {
-        string name;
-        int age;
-        char gender;
-        double height;
-        double weight;
+        try {
+            string name;
+            int age;
+            char gender;
+            double height;
+            double weight;
 
-        cout << "Enter name: ";
-        cin >> name;
+            cout << "Enter name: ";
+            cin >> name;
 
-        cout << "Enter age: ";
-        cin >> age;
+            cout << "Enter age: ";
+            cin >> age;
+            if (age <= 0) {
+                throw std::invalid_argument("Invalid age. Please enter a positive value.");
+            }
 
-        cout << "Enter gender (M/F): ";
-        cin >> gender;
+            cout << "Enter gender (M/F): ";
+            cin >> gender;
+            if (gender != 'M' && gender != 'F') {
+                throw std::invalid_argument("Invalid gender. Please enter 'M' or 'F'.");
+            }
 
-        cout << "Enter height (cm): ";
-        cin >> height;
+            cout << "Enter height (cm): ";
+            cin >> height;
+            if (height <= 0) {
+                throw std::invalid_argument("Invalid height. Please enter a positive value.");
+            }
 
-        cout << "Enter weight (kg): ";
-        cin >> weight;
+            cout << "Enter weight (kg): ";
+            cin >> weight;
+            if (weight <= 0) {
+                throw std::invalid_argument("Invalid weight. Please enter a positive value.");
+            }
 
-        User user(name, age, gender, height, weight);
-        system.addUser(user);
+            User user(name, age, gender, height, weight);
+            system.addUser(user);
+        }
+        catch (const std::exception& e) {
+            cout << "Error: " << e.what() << endl;
+        }
     }
 
     void displayReport() {
